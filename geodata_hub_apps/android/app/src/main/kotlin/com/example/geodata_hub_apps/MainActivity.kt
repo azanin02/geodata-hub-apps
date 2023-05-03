b3e7223ed347febe04f58ea82559f1a30c230b0c
Abstract Programmer <AbstractProgrammer@example.com>
2023-05-03 21:12:07 +0800

    first commit

---
+++ geodata_hub_apps/android/app/src/main/kotlin/com/example/geodata_hub_apps/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.geodata_hub_apps
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ geodata_hub_apps/lib/country.dart
@@ -0,0 +1,6 @@
+class Country {
+  var place = "Not available", currency = "Not available", flag = "https://flagsapi.com/MY/flat/64.png", 
+  capital = "Not available";
+
+  Country(this.place, this.currency, this.flag, this.capital);
+}
